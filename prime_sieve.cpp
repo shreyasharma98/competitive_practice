8071f82d8acf3f0e74e9e1b496495d875624c31a
Shreya Sharma <35392800+shreyasharma98@users.noreply.github.com>
2020-01-30 02:09:41 +0530

    Update prime_sieve.cpp

--- prime_sieve.cpp
+++ prime_sieve.cpp
@@ -1,41 +1,51 @@
 #include<iostream>
 using namespace std;
+
 #define ll long long
-void prime_s(int *p)
-{
-    for(int i=3;i<1000000;i+=2)
-    {
-        p[i] = 1;
-    }
-    for(ll i=3;i<1000000;i+=2){
-        if(p[i] == 1)
-    {
-        for(ll j=i*i ;i<1000000;j+=i)
-        {
-            p[j] =0;
-        }
-    }
-    }
-    p[0] = p[1] = 0;
-    p[2] = 1;
+
+void prime_sieve(int *p) {
+
+	//first mark all odd number's prime
+	for (int i = 3; i <= 1000000; i += 2) {
+		p[i] = 1;
+	}
+
+	// Sieve
+	for (ll i = 3; i <= 1000000; i += 2) {
+		//if the current number is not marked (it is prime)
+		if (p[i] == 1) {
+			//mark all the multiples of i as not prime
+			for (ll j = i * i; j <= 1000000; j = j + i ) {
+				p[j] = 0;
+			}
+		}
+
+	}
+	//special case
+	p[2] = 1;
+	p[1] = p[0] = 0;
+
 }
 
-int main() {
-    int t;
-    cin>>t;
-    int p[1000000] = {0};
-    prime_s(p);
 
-	int csum[1000000] = {0};
-	for(int i =1;i<=1000000;i++)
-	{
-		csum[i] = csum[i-1] + p[i];
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int p[1000005] = {0};
+	prime_sieve(p);
+
+	//lets print primes upto range n
+	for (int i = 0; i <= n; i++) {
+		if (p[i] == 1) {
+			cout << i << " ";
+		}
 	}
-    while(t--)
-    {
-        int n,m ;
-        cin>>n>>m;
-		cout<<csum[m] - csum[n-1]<<endl;
-    }
+
+
+
+
 	return 0;
 }
